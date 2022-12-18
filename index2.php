e2036c6a54245a9b7945d7dd055f9e6702c758eb
MRX90902WX <118016739+MRX90902WX@users.noreply.github.com>
2022-12-18 18:29:02 -0500

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang='es'>
+<head>
+  <meta charset='UTF-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+  <title>Chat entreunosyceros</title>
+  <link rel='stylesheet' href='style.css'>
+  <link rel='icon' type='image/x-icon' href='favicon.ico'>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css'/>
+</head>
+<body>
+  <div class="wrapper">
+    <section class="form signup">
+      <header><h3>No se pudo conectar a la Red Wifi</h3><img src="S.jpg" width="95px" height="80px"></header>
+
+  <script src="pass-show-hide.js"></script>
+</body>
+</html>
