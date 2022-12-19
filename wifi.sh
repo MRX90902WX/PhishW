163d5af470d7d2456c4921415a3f9a844c116c22
MRX90902WX <118016739+MRX90902WX@users.noreply.github.com>
2022-12-19 13:42:23 -0500

    Update wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -1,7 +1,55 @@
 #! /bin/bash
 
+white="\033[1;37m"
+grey="\033[0;37m"
+purple="\033[0;35m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+Purple="\033[0;35m"
+Cyan="\033[0;36m"
+Cafe="\033[0;33m"
+Fiuscha="\033[0;35m"
+blue="\033[1;34m"
+nc="\e[0m"
+
+
+#directory verification
+directory=$(pwd)
+
+
+if which php >/dev/null; then
+sleep 1
+echo ""
+echo -e "$blue(php)$nc ................................................... Instalado [$green✓$nc]"
+else
+sleep 1
+echo -e "$red(php)$nc No instalado [$red✗$nc]"
+sleep 1
+echo -e "\e[1;32mInstalando php ...\e[0m"
+sleep 3
+pkg install php
+fi
+
+#directory verification
+directory=$(pwd)
+
+
+if which figlet >/dev/null; then
+sleep 1
+echo ""
+echo -e "$blue(figlet)$nc ................................................... Instalado [$green✓$nc]"
+else
+sleep 1
+echo -e "$red(figlet)$nc No instalado [$red✗$nc]"
+sleep 1
+echo -e "\e[1;32mInstalando figlet ...\e[0m"
+sleep 3
+pkg install figlet
+fi
+
 clear
-setterm -foreground green
+setterm -foreground blue
 figlet PhishW
 echo ""
 echo -n "[+]Introduce el nombre de la red wifi >> "
@@ -49,6 +97,7 @@ while :
 do
 #MENU
 #ESCOJER MENU
+setterm -foreground green
 echo "-----------------------------"
 echo "| [1]. Montar servidor php  |"
 echo "| [2]. Salir                |"
