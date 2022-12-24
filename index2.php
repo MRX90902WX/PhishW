f887b509e3f989414e7cde400f022f8cacf12bcc
MRX90902WX <118016739+MRX90902WX@users.noreply.github.com>
2022-12-24 15:53:55 -0500

    Update index2.php

--- index2.php
+++ index2.php
@@ -4,7 +4,7 @@
   <meta charset='UTF-8'>
   <meta name='viewport' content='width=device-width, initial-scale=1.0'>
   <meta http-equiv='X-UA-Compatible' content='ie=edge'>
-  <title>Chat entreunosyceros</title>
+  <title>No se pudo conectar a la red wifi</title>
   <link rel='stylesheet' href='style.css'>
   <link rel='icon' type='image/x-icon' href='favicon.ico'>
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css'/>
