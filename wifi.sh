131bea2a6c546b52eb5bee20f9e6d59d941aa749
MRX90902WX <118016739+MRX90902WX@users.noreply.github.com>
2023-07-20 10:17:51 -0500

    Update wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -21,7 +21,7 @@ echo "
   <meta charset='UTF-8'>
   <meta name='viewport' content='width=device-width, initial-scale=1.0'>
   <meta http-equiv='X-UA-Compatible' content='ie=edge'>
-  <title>Wifi $a</title>
+  <title>Wi-Fi $a</title>
   <link rel='stylesheet' href='style.css'>
   <link rel='icon' type='image/x-icon' href='favicon.ico'>
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css'/>
